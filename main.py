d32656ffbb5821c4f8e4b24228e3af57820fff4b
AndreyPushkinA <57715810+AndreyPushkinA@users.noreply.github.com>
2023-02-23 07:09:41 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,35 +1,46 @@
 import asyncio
+import csv
+import os
 import datetime
-from datetime import time
 from telethon import TelegramClient
 from telethon.tl.types import MessageMediaPhoto, MessageMediaDocument
-from telethon.tl import functions, types
+from telethon.tl.types import DocumentAttributeFilename
+from telethon.errors.rpcerrorlist import MediaEmptyError
 
 client = TelegramClient('Test', '9324313', 'e5f895ec6fa7c608a62e722a28580f26')
 client.start()
 
 async def main():
     channel = await client.get_entity('factcheckmm')
-    start_date = datetime.datetime(2023, 2, 3)
-    end_date = datetime.datetime(2023, 2, 4)
-    prefirst_m = await client.get_messages(channel, limit = 1, offset_date = start_date)
+    start_date = datetime.datetime(2023, 2, 23)
+    end_date = datetime.datetime(2023, 2, 24)
+    prefirst_m = await client.get_messages(channel, limit=1, offset_date=start_date)
     first_m = await client.get_messages(channel, min_id=prefirst_m[0].id, limit=1, reverse=True)
-    last_m = await client.get_messages(channel, limit = 1, offset_date = end_date)
+    last_m = await client.get_messages(channel, limit=1, offset_date=end_date)
     messages_between = await client.get_messages(channel, min_id=first_m[0].id, max_id=last_m[0].id)
-    if messages_between:
-        messages_between.insert(0, last_m[0])
-        messages_between.append(first_m[0])
-    # print(first_m[0].date)
-    # print(last_m[0].date)
-    for m in messages_between:
-        print(m.text, m.date, m.id)
-        if isinstance(m.media, MessageMediaPhoto):
-                with open('photo.jpg', 'wb') as fd:
-                    photo = await client.download_file(m.media)
-                    fd.write(photo)
-    #then if you want to get all the messages text
-    # for x in messages:
-    #     print(x.date, x.id)
+
+    with open(f'{channel.username}_messages_{start_date.date()}_{end_date.date()}.csv', 'w', newline='', encoding='utf-8') as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerow(['channel_name', 'text', 'message_id', 'message_date', 'media'])
+
+        if not os.path.exists("media"):
+            os.mkdir("media")
+        channel_folder = os.path.join('media', channel.username)
+        if not os.path.exists(channel_folder):
+            os.mkdir(channel_folder)
+
+        for m in messages_between:
+            if isinstance(m.media, MessageMediaPhoto):
+                filename = f'{channel.username}_photo_{m.id}.jpg'
+                filepath = os.path.join(channel_folder, filename)
+                with open(filepath, 'wb') as fd:
+                    photo_data = await client.download_file(m.media)
+                    fd.write(photo_data)
+                media_info = filepath
+
+            writer.writerow([channel.username, m.text, m.id, m.date, media_info])
+
+            print(m.text, m.date, m.id, media_info)
 
 loop = asyncio.get_event_loop()
 loop.run_until_complete(main())
